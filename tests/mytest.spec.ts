3d8d9f3fa80c44fabf036dc71a158160c003da6a
Avinash Doifode <avinash.doifode@globant.com>
2026-06-09 15:23:33 +0530

    test3 added

--- tests/mytest.spec.ts
+++ tests/mytest.spec.ts
@@ -3,21 +3,21 @@ import {test,expect,Locator} from '@playwright/test';
 test('validate login',async({page})=>{
     
     await page.goto('');
-     const [download]=Promise.all([ page.waitForEvenet('download'), 
+     //const [download]=Promise.all([ page.waitForEvenet('download'), 
          page.locator('#name').click()]);
 
          const downloadpath="download/demo.txt";
 
-         download.saveAs(downloadpath);
+        // download.saveAs(downloadpath);
 
-         const path=await download.path();
+        // const path=await download.path();
          console.log(path);
 
-         await page.locator('#name').setInputFiles('download/demo.txt');
+        // await page.locator('#name').setInputFiles('download/demo.txt');
 
-         page.on('dialog',(dialog)=>{
-            dialog.accept();
-            const dm:String=dialog.message();
+       //  page.on('dialog',(dialog)=>{
+          //  dialog.accept();
+           // const dm:String=dialog.message();
             console.log(dm);
 
             await expect(dm).not.toBeFalsy(); fe
