abd753543ddd041de261aa914a9c757e7a54c586
Avinash Doifode <avinash.doifode@globant.com>
2026-06-09 13:24:42 +0530

    one change is validation

--- tests/mydemotest.spec.ts
+++ tests/mydemotest.spec.ts
@@ -11,4 +11,5 @@ test('test', async ({ page }) => {
     - link "DM SPECIALLY FOR SPECIALIST HDPE Disposable Poly Gloves...":
       - /url: /dm-specially-specialist-hdpe-disposable-poly-gloves-100-pcs-food-grade-transparent-wet-dry-glove-set/p/itmaab98003dda84?pid=CLGHH2GPDYKWN7DA&lid=LSTCLGHH2GPDYKWN7DAAMQHRB&marketplace=FLIPKART&store=rja%2Fz2d&srno=b_1_3&otracker=browse&fm=neo%2Fmerchandising&iid=en_QsCiZZFQkLgYgqwCU3fVTy72C0ck2q0KLP5Evaa02yppXdgDaA2PlZCNZxfuOJ8cg9_yx5takpS7NlTOAQN_PA%3D%3D&ppt=browse&ppn=browse&ssid=78tl6eyyj40000001780900278158&ov_redirect=true&ov_redirect=true
     `);
+    await page.waitForTimeout(3000);
 });
